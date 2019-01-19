f319a73a59b1b1201b7636d35b086a21e12c719b
Photon <39137120+phooton@users.noreply.github.com>
2019-01-19 15:10:31 +0100

    Fix 6GB auto-detection

--- src/GrinGoldMinerCLI/Program.cs
+++ src/GrinGoldMinerCLI/Program.cs
@@ -102,7 +102,7 @@ namespace Mozkomor.GrinGoldMinerCLI
 
                     foreach (var dev in devices)
                     {
-                        if (dev.memory > (long)1024 * 1024 * 1024 * 7)
+                        if (dev.memory > (long)1024 * 1024 * 1024 * 6)
                         {
                             generated_config.GPUOptions.Add(new SharedSerialization.GPUOption()
                             {
